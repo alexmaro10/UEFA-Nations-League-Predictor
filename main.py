f099bf228c31893dd45146c68fbb75756f95ef0c
Alejandro <alexmape40@gmail.com>
2026-09-22 11:25:56 +0200

    Nombres actualizados

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ app.add_middleware(
 
 app.include_router(router)
 
-N_SIMULATIONS = 100.000
-N_SAVED_SIMULATIONS = 300
+N_SIMULATIONS = 100
+N_SAVED_SIMULATIONS = 30
 
 def clean_previous_simulations():
 	simulations_dir = "data/simulations"
